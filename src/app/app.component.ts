4e52d4c4b15ee2cbae05eab7304beddbf86dea5a
jandie <eidnaj@live.nl>
2018-05-24 11:23:56 +0200

    Added links to toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LoginService} from './services/login/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Kwetter';
+
+  constructor(public loginService: LoginService) {}
+
+  logout() {
+    this.loginService.token = null;
+    this.loginService.loggedUser = null;
+    localStorage.setItem('token', null);
+  }
 }
